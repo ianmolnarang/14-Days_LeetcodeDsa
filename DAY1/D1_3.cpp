5bde238ef015e047221f6d37a6161b39f4fd6324
Anmol Narang <nanmol0406@gmail.com>
2022-05-14 22:56:33 +0530

    Update

--- DAY1/D1_3.cpp
+++ DAY1/D1_3.cpp
@@ -8,13 +8,3 @@ public:
         while (left <= right)
         {
             pivot = left + (right - left) / 2;
-            if (nums[pivot] == target)
-                return pivot;
-            if (target < nums[pivot])
-                right = pivot - 1;
-            else
-                left = pivot + 1;
-        }
-        return -1;
-    }
-};
