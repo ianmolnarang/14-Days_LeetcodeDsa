69768fae75de620fbc09c4d3f5c3ba7abd8dae06
Anmol Narang <nanmol0406@gmail.com>
2024-01-05 23:11:18 +0530

    enjoy

---
+++ DAY3 - Copy/1.cpp
@@ -0,0 +1,50 @@
+// MOve Zeroes 283
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 0 || n == 1)
+            return;
+        int left = 0;
+        int right = 0;
+        int temp;
+        while (right < n)
+        {
+            if (nums[right] == 0)
+            {
+                right++;
+            }
+            else
+            {
+                temp = nums[left];
+                nums[left] == nums[right];
+                nums[right] = temp;
+                left++;
+                right++;
+            }
+        }
+    }
+};
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --class Solution
+{
+public
+    void moveZeroes(int[] nums)
+    {
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                // swap
+                int t = nums[i];
+                nums[i] = nums[left];
+                nums[left] = t;
+
+                left++;
+            }
+        }
+    }
+}
